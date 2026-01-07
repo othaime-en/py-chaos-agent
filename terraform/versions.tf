4f84b9482513630a46b2bb0c56e7e6dff02e25c8
othaime-en <61498231+othaime-en@users.noreply.github.com>
2026-01-07 04:17:11 +0300

    feat(k8s): update aws and helm versions in terraform/

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.12"
+      version = "~> 3.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
